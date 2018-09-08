ae55f289960a88e24a08deec932b090e2658cf4d
Sergey Zaharchenko <zaharchenko.dev@gmail.com>
2018-09-08 04:47:03 +0000

    Test data added

---
+++ public/test/index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <script
+            src="https://code.jquery.com/jquery-3.3.1.min.js"
+            integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+            crossorigin="anonymous"></script>
+</head>
+<body>
+<div id="links">
+    <a href="1.html">First link</a>
+    <a href="2.html">Second link</a>
+</div>
+
+
+</body>
+</html>
